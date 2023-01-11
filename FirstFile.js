db9b7bb1304f4989caae26719fe9b21b5a6449c6
Ashish <ash.ishu025@gmail.com>
2023-01-11 22:03:25 +0530

    first file

--- FirstFile.js
+++ FirstFile.js
@@ -1,5 +1,17 @@
-//ctrl + ~
-//REPL - Read Evaluate Print and Loop
-for (let index = 0; index < 10000; index++) {
-    console.log(index);    
+//core javascript/ vanilla js
+//variable declaration
+
+var sessionName = "MERNStack Session", month = "January"
+
+console.log("session Name is - ", sessionName +" "+ month)
+
+//function keyword 
+
+function PrintSession(sessionName) {
+    console.log("Session Name from function - ", sessionName)
 }
+
+// Area are = new Area();
+// Area.printRectangle();
+
+PrintSession("Core Javascript");
