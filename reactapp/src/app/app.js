eb6e4a9964375c09eb576ccc3f1fb488a8d2bbad
Ashish <ash.ishu025@gmail.com>
2023-02-10 23:06:55 +0530

    react

--- reactapp/src/app/app.js
+++ reactapp/src/app/app.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+
+export class ApplicationComponent extends Component{
+
+    constructor(props){
+        super();
+        this.state = {
+            title : "Application Component Title"
+        }
+
+        //this.titleChange();
+    }
+
+    titleChange = setInterval(() => {
+        this.setState({
+            title : "Change Title after 5 seonds"
+        })
+        console.log(this.state.title);
+    }, 5000);
+
+    //virtual dom of component
+    render(){
+        return(
+            <div>
+                <label>From Application Class Component</label>
+                <h4>{this.state.title}</h4>
+            </div>
+        )
+    }
+
+}
+
 
 let DefaultApp = ()=>{
     let firstValue = 25;
     let secondValue = 25;
+    let title = "My Second React Application";
     return(
         <>
-            <h2>My First React Application</h2>
+            <h2>{title}</h2>
             <label>Sum Is : {firstValue+ secondValue}</label>
         </>
     )
