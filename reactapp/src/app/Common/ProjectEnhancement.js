a8da20457fed1d6282dd45cca13f8bac3e0f5c1b
Ashish <ash.ishu025@gmail.com>
2023-02-28 21:08:26 +0530

    practice question

---
+++ reactapp/src/app/Common/ProjectEnhancement.js
@@ -0,0 +1,5 @@
+// 28-02-2023 - Other Practice Questions
+// Practice - 1
+
+//1. Create a component with name Hobby, add a textbox to insert hobbyname into it and save it to the database using SaveAPI
+//2. Create an action to save the hobby, can use useractions file and userrouter file for the same
