5889f2c1b9f99627c1756abc8748025895d8bcb1
Ashish <ash.ishu025@gmail.com>
2023-03-01 21:49:37 +0530

    add product component

--- reactapp/src/app/Common/ProjectEnhancement.js
+++ reactapp/src/app/Common/ProjectEnhancement.js
@@ -1,3 +1,12 @@
+// 01-03-2023 - Create Product and Display Them
+
+// Create a product component using functional component and hooks (try using use state to read default values from reducer)
+// Create a form to allow user to submit Product Details - name, price, desc, rating - textboxes and button
+// Create an action method to add the detail to database using a server api - api/saveproduct
+// Server Side - Create product router and api to save the product using productdatamodel
+// Then create and API to fetch the products and displaye them on Another component - DisplayProducts
+
+
 // 28-02-2023 - Other Practice Questions
 // Practice - 1
 
