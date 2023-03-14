5ff7fc28e04a3fd76d8e8fd112de8a60d2e22553
Ashish <ash.ishu025@gmail.com>
2023-03-14 21:17:41 +0530

    re-order and recent order

--- reactapp/src/app/Common/ProjectEnhancement.js
+++ reactapp/src/app/Common/ProjectEnhancement.js
@@ -1,3 +1,18 @@
+// 15-01-2023 - Practice Question
+
+// Reorder
+// User to reorder from recent orders or from cancelled orders
+// A Simple process just add the order to your cart and replace or merge whatever is present in cart
+
+
+// 14-01-2022 - Recentorder Page 
+// Upon MakePayment Click
+// Save the cart to RecentOrders collection (should have userid, order, dateTime)
+// Make API to Save and Fetch from RecentOrders
+// Make a component RecentOrders to Show all previous Orders of current user
+// Add a button to Cancel (like) we have remove in CartComponent and then save again, 
+// order can be cancelled within 2 days (48 hrs) after that it should be marked delivered
+
 // 13-03-2023 - Practice
 
 // Coupon Page 
